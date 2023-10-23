ddac20dcfe5ec7ac229642e8537de09974687754
leinson <hannah.leinson@gmail.com>
2023-10-23 15:41:06 +0300

    exercises 3.15-3.18

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const cors = require("cors");
 
 const Person = require("./models/person");
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
 morgan.token("postContent", (req) =>
   req.method === "POST" ? JSON.stringify(req.body) : ""
 );
@@ -18,53 +31,17 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms :postContent"
   )
 );
-
-let persons = [
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 6,
-  },
-  {
-    name: "Arto Hellas",
-    number: "0401020304",
-    id: 9,
-  },
-];
+app.use(errorHandler);
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  if (!body.number && !body.name) {
+  if (!body.number || !body.name) {
     return res.status(400).json({
-      error: "name and number missing",
+      error: "missing content",
     });
   }
-  if (!body.name) {
-    return res.status(400).json({
-      error: "name missing",
-    });
-  }
-  if (!body.number) {
-    return res.status(400).json({
-      error: "number missing",
-    });
-  }
-  if (persons.some((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: "name already exists",
-    });
-  }
+
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -86,28 +63,52 @@ app.get("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    if (!person) {
-      return res.status(400).json({
-        error: "no such id",
-      });
-    }
-    res.json(person);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        return res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+      console.log("deleted!");
+    })
+    .catch((error) => next(error));
+});
 
-  res.status(204).end();
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  console.log("req body", body);
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
-  let person_amount = `Phonebook has info for ${persons.length} people`;
-  res.send(person_amount + "<br><br>" + new Date());
+  Person.find({}).then((persons) => {
+    const person_amount = `Phonebook has info for ${persons.length} people`;
+    res.send(person_amount + "<br><br>" + new Date());
+    console.log("personamountt:", person_amount);
+  });
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
